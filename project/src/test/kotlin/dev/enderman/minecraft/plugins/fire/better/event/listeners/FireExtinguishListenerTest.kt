ffd9ab82c7977ed7ff895d8659b40a384beaa5da
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-18 11:24:04 +0000

    Add "missing" test
    IDK, but jacoco was complaining.

--- project/src/test/kotlin/dev/enderman/minecraft/plugins/fire/better/event/listeners/FireExtinguishListenerTest.kt
+++ project/src/test/kotlin/dev/enderman/minecraft/plugins/fire/better/event/listeners/FireExtinguishListenerTest.kt
@@ -11,6 +11,7 @@ import org.mockbukkit.mockbukkit.world.Coordinate
 import org.mockbukkit.mockbukkit.world.WorldMock
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
 
 class FireExtinguishListenerTest : AbstractInflamityPluginTest() {
@@ -32,6 +33,16 @@ class FireExtinguishListenerTest : AbstractInflamityPluginTest() {
 
         assertTrue(player.fireTicks > 0, "Player should be on fire after attempting to put out fire with bare hands.")
         assertEquals(fire.type, Material.FIRE, "Fire should remain after attempting to put out fire with bare hands.")
+    }
+
+    @Test fun `player does not ignite when block is not fire`() {
+        val notFire = world.getBlockAt(Location(world, 0.0, 0.0, 0.0))
+        notFire.setType(Material.OAK_LOG)
+
+        player.breakBlock(notFire)
+
+        assertTrue(player.fireTicks <= 0, "Player should be able to break non-fire blocks normally.")
+        assertNotEquals(notFire.type, Material.FIRE, "Non-fire block does not turn to fire after player breaks it.")
     }
 
     @Test fun `event listener works`() {
