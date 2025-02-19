7b5ae7564e8b1d3f7e0a60abbf5a79ec55ec0e76
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-19 13:40:38 +0000

    Use non-experimental API

--- project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/fire/IsFireDamage.kt
+++ project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/fire/IsFireDamage.kt
@@ -1,15 +1,15 @@
 package dev.enderman.minecraft.plugins.fire.better.events.fire
 
-import org.bukkit.damage.DamageType
 import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause
 
 val fireDamageTypes = listOf(
-    DamageType.IN_FIRE,
-    DamageType.ON_FIRE,
-    DamageType.CAMPFIRE,
+    DamageCause.FIRE,
+    DamageCause.FIRE_TICK,
+    DamageCause.CAMPFIRE,
 //    DamageType.HOT_FLOOR
 )
 
 fun EntityDamageEvent.isFireDamage(): Boolean {
-    return damageSource.damageType in fireDamageTypes
+    return cause in fireDamageTypes
 }
