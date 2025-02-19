1de7aba1cc39ad3e533133ba9b8c31b45544715b
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-19 21:55:06 +0000

    Update entity fire spread test

--- project/src/test/kotlin/dev/enderman/minecraft/plugins/fire/better/event/listeners/EntityFireSpreadListenerTest.kt
+++ project/src/test/kotlin/dev/enderman/minecraft/plugins/fire/better/event/listeners/EntityFireSpreadListenerTest.kt
@@ -1,6 +1,7 @@
 package dev.enderman.minecraft.plugins.fire.better.event.listeners
 
 import dev.enderman.minecraft.plugins.fire.better.AbstractInflamityPluginTest
+import dev.enderman.minecraft.plugins.fire.better.events.fire.fireDamageTypes
 import dev.enderman.minecraft.plugins.fire.better.events.listeners.EntityFireSpreadListener
 import org.bukkit.Material
 import org.bukkit.World
@@ -8,6 +9,7 @@ import org.bukkit.block.Block
 import org.bukkit.damage.DamageSource
 import org.bukkit.damage.DamageType
 import org.bukkit.entity.Entity
+import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Sheep
 import org.bukkit.event.entity.EntityDamageEvent
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -19,7 +21,7 @@ import kotlin.test.assertNotEquals
 
 class EntityFireSpreadListenerTest : AbstractInflamityPluginTest() {
     private lateinit var world: World
-    private lateinit var entity: Entity
+    private lateinit var entity: LivingEntity
     private lateinit var startingBlock: Block
 
     @BeforeTest fun setUpEnvironment() {
@@ -28,39 +30,43 @@ class EntityFireSpreadListenerTest : AbstractInflamityPluginTest() {
         startingBlock = entity.location.block
     }
 
-    @Test fun `lit entities spread fire`() {
-        entity.fireTicks = 100
+    @Test fun `lit entities spread fire (event)`() {
+        for (damageType in fireDamageTypes) {
+            setUpEnvironment()
 
-        server.scheduler.runTaskLater(plugin, { ->
-            assertEquals(startingBlock.type, Material.FIRE)
-        }, 100L)
+            val damage = 1.0
+            entity.damage(damage)
+
+            val event = EntityDamageEvent(
+                entity,
+                damageType,
+                DamageSource.builder(DamageType.GENERIC).withDamageLocation(entity.location).build(),
+                damage
+            )
+            event.callEvent()
+
+            server.scheduler.performTicks(100L)
+
+            assertEquals(Material.FIRE, startingBlock.type, "Lit entity should spread fire after being damaged by fire.")
+        }
     }
 
     @Test fun `unlit entities do not spread fire`() {
-        server.scheduler.runTaskLater(plugin, { ->
-            assertEquals(startingBlock.type, Material.FIRE)
-        }, 100L)
+        server.scheduler.performTicks(100L)
+
+        assertNotEquals(Material.FIRE, startingBlock.type)
     }
 
-    @Test fun `event listener works`() {
-        assertDoesNotThrow("Event listener should not throw any errors.") {
-            val listener = EntityFireSpreadListener()
+    @Test fun `non-fire damage does not spread fire (event)`() {
+        val event = EntityDamageEvent(
+            entity,
+            EntityDamageEvent.DamageCause.SUICIDE,
+            DamageSource.builder(DamageType.GENERIC).build(),
+            1.0
+        )
 
-            val event = EntityDamageEvent(entity, EntityDamageEvent.DamageCause.FIRE, DamageSource.builder(DamageType.ON_FIRE).build(), 1.0)
+        event.callEvent()
 
-            listener.onEntityBurn(event)
-        }
-    }
-
-    @Test fun `event listener works for non-fire damage`() {
-        assertDoesNotThrow("Event listener should not throw any errors when handling non-fire damage.") {
-            val listener = EntityFireSpreadListener()
-
-            val event = EntityDamageEvent(entity, EntityDamageEvent.DamageCause.SUICIDE, DamageSource.builder(DamageType.GENERIC).build(), 1.0)
-
-            listener.onEntityBurn(event)
-
-            assertNotEquals(Material.FIRE, startingBlock.type, "Block should not be on fire when entity is not on fire.")
-        }
+        assertNotEquals(Material.FIRE, startingBlock.type, "Block should not be on fire when entity is not on fire.")
     }
 }
