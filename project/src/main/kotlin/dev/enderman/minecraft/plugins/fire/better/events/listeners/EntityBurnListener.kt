f80dd49e4180a168b63fe5f8be7a688120174ecd
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-20 15:45:59 +0000

    Make fire protection a lot more powerful
    * Now scales fire damage, with a full set making you completely immune to fire damage.

--- project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/listeners/EntityBurnListener.kt
+++ project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/listeners/EntityBurnListener.kt
@@ -1,6 +1,8 @@
 package dev.enderman.minecraft.plugins.fire.better.events.listeners
 
 import dev.enderman.minecraft.plugins.fire.better.events.fire.isFireDamage
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.entity.LivingEntity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
@@ -16,6 +18,32 @@ class EntityBurnListener : Listener {
         }
 
         if (!event.isFireDamage()) return
+
+        if (entity is LivingEntity) {
+            val equipment = entity.equipment
+
+            val head = equipment?.helmet
+            val chest = equipment?.chestplate
+            val legs = equipment?.leggings
+            val feet = equipment?.boots
+
+            val headLevel = head?.itemMeta?.getEnchantLevel(Enchantment.FIRE_PROTECTION) ?: 0
+            val chestLevel = chest?.itemMeta?.getEnchantLevel(Enchantment.FIRE_PROTECTION) ?: 0
+            val legsLevel = legs?.itemMeta?.getEnchantLevel(Enchantment.FIRE_PROTECTION) ?: 0
+            val feetLevel = feet?.itemMeta?.getEnchantLevel(Enchantment.FIRE_PROTECTION) ?: 0
+
+            val total = headLevel + chestLevel + legsLevel + feetLevel
+
+            if (total == 16) {
+                event.isCancelled = true
+                entity.fireTicks = 0
+                return
+            }
+
+            val damageFactor = (16.0 - total) / 16.0
+
+            event.damage *= damageFactor
+        }
 
         entity.fireTicks = 10_000
     }
