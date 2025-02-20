1e3c6f414f4c8f5d2e216019003c7eeac4114ecb
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-20 15:31:38 +0000

    Don't use experimental API

--- project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/listeners/EntityBurnListener.kt
+++ project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/listeners/EntityBurnListener.kt
@@ -1,18 +1,15 @@
 package dev.enderman.minecraft.plugins.fire.better.events.listeners
 
 import dev.enderman.minecraft.plugins.fire.better.events.fire.isFireDamage
-import org.bukkit.damage.DamageType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
 
 class EntityBurnListener : Listener {
     @EventHandler fun onEntityBurn(event: EntityDamageEvent) {
-        val source = event.damageSource
-
         val entity = event.entity
 
-        val isSuffocating = source.damageType == DamageType.IN_WALL
+        val isSuffocating = event.cause == EntityDamageEvent.DamageCause.SUFFOCATION
         if (isSuffocating) {
             entity.fireTicks = 0
             return
