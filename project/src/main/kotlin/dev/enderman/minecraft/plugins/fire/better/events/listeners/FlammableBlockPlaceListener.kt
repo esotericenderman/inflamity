b5129a8d3161c32bffbb79b77988c0cdb44760de
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-27 13:12:21 +0000

    Spread fire around placed flammable blocks

--- project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/listeners/FlammableBlockPlaceListener.kt
+++ project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/listeners/FlammableBlockPlaceListener.kt
@@ -1,6 +1,8 @@
 package dev.enderman.minecraft.plugins.fire.better.events.listeners
 
 import dev.enderman.minecraft.plugins.fire.better.FIRE_DURATION
+import dev.enderman.minecraft.plugins.fire.better.fire.attemptFireSpread
+import dev.enderman.minecraft.plugins.fire.better.utility.block.directions
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockPlaceEvent
@@ -16,5 +18,11 @@ class FlammableBlockPlaceListener : Listener {
         if (!type.isAir && !type.isFlammable && !type.isFuel && !type.isBurnable) return
 
         event.player.fireTicks = FIRE_DURATION
+
+        directions.forEach {
+            val blockInDirection = block.getRelative(it)
+
+            blockInDirection.attemptFireSpread()
+        }
     }
 }
