475bcd4935081337635bbe6016c671e13f21510c
Esoteric Enderman <esotericenderman@gmail.com>
2025-02-28 16:27:46 +0000

    Prevent thrown fireballs from setting fire

--- project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/listeners/FirePlaceListener.kt
+++ project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/listeners/FirePlaceListener.kt
@@ -4,6 +4,7 @@ import dev.enderman.minecraft.plugins.fire.better.fire.supportsFire
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockIgniteEvent
 import org.bukkit.event.block.BlockPlaceEvent
 
 class FirePlaceListener : Listener {
@@ -11,6 +12,15 @@ class FirePlaceListener : Listener {
     private fun onFirePlace(event: BlockPlaceEvent) {
         val block = event.block
         if (block.type != Material.FIRE) return
+
+        if (!block.supportsFire()) {
+            event.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    private fun onFirePlace(event: BlockIgniteEvent) {
+        val block = event.block
 
         if (!block.supportsFire()) {
             event.isCancelled = true
