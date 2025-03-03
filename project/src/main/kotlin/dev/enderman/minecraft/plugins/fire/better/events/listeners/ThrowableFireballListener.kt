2b917837b4095e57a4c1c254e0c2ff62891c7c9e
Esoteric Enderman <esotericenderman@gmail.com>
2025-03-03 21:08:19 +0000

    Play sound when throwing fireball

--- project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/listeners/ThrowableFireballListener.kt
+++ project/src/main/kotlin/dev/enderman/minecraft/plugins/fire/better/events/listeners/ThrowableFireballListener.kt
@@ -2,6 +2,7 @@ package dev.enderman.minecraft.plugins.fire.better.events.listeners
 
 import dev.enderman.minecraft.plugins.fire.better.gameModesWithConsequences
 import org.bukkit.Material
+import org.bukkit.Sound
 import org.bukkit.entity.SmallFireball
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -29,6 +30,7 @@ class ThrowableFireballListener : Listener {
         player.launchProjectile(SmallFireball::class.java)
 
         player.swingHand(EquipmentSlot.HAND)
+        player.playSound(player, Sound.ITEM_FIRECHARGE_USE, 1.0F, 1.0F)
 
         if (!gameModesWithConsequences.contains(player.gameMode)) return
 
